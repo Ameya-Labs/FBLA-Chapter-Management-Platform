c0563980640fb7c455f1763bc8735b45da7b0c2c
Ameya Jadhav <jadhavameyak@gmail.com>
2022-09-03 15:49:53 -0400

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,18 +1,18 @@
 const APPLICATION_VARIABLES = {
 
   // General Info
-  APP_NAME:"Denmark FBLA",
-  APP_LINK:"https://denmarkfbla.web.app",
-  CHAPTER_WEBSITE:"https://denmarkhsfbla.weebly.com/",
-  LANDING_PAGE_DESCRIPTION: "Denmark’s Future Business Leaders of America (FBLA) is Denmark High School’s premier business and computer science student-led organization, driven to help students reach their goals in the professional world. Today, Denmark FBLA stands strong as an organization of over 300 talented individuals who strive to make their mark by shaping a brighter future for their community.",
-  DEFAULT_PASSWORD:"DenmarkFBLA123",
+  APP_NAME:"FBLA Chapter",
+  APP_LINK:"https://ameyalabs-fbla.web.app",
+  CHAPTER_WEBSITE:"https://fblachapterwebsite.com/",
+  LANDING_PAGE_DESCRIPTION: "Short chapter description or promotional text here.",
+  DEFAULT_PASSWORD:"FBLAChapter123",
 
   // Socials (leave blank if chapter doesn't have one)
-  CHAPTER_INSTAGRAM_URL: "https://www.instagram.com/denmarkfbla/",
-  CHAPTER_TWITTER_URL: "https://twitter.com/DenmarkFbla",
-  CHAPTER_FACEBOOK_URL: "https://www.facebook.com/denmark.fbla.3",
-  CHAPTER_YOUTUBE_URL: "https://www.youtube.com/channel/UCKxk1NEI3KA1i4PtdAQQvsQ",
-  CHAPTER_CONTACT_EMAIL: "denmark.hs.fbla@gmail.com",
+  CHAPTER_INSTAGRAM_URL: "https://www.instagram.com/",
+  CHAPTER_TWITTER_URL: "https://twitter.com/",
+  CHAPTER_FACEBOOK_URL: "https://www.facebook.com/",
+  CHAPTER_YOUTUBE_URL: "https://www.youtube.com/channel/",
+  CHAPTER_CONTACT_EMAIL: "fblachapter@gmail.com",
 
   // Platform Coloring
   NAV_BAR_COLOR:"#495867",
@@ -32,9 +32,9 @@ const APPLICATION_VARIABLES = {
 
   // Help Information
   SUPPORT_EMAIL:"ajadhav@stanford.edu",
-  SUPPORT_EMAIL_SUBJECT:"Technical Support for Denmark FBLA App",
-  GENERAL_EMAIL:"denmark.hs.fbla@gmail.com",
-  GENERAL_EMAIL_SUBJECT:"General Support for Denmark FBLA App",
+  SUPPORT_EMAIL_SUBJECT:"Technical Support for FBLA Chapter App",
+  GENERAL_EMAIL:"fblachapter@gmail.com",
+  GENERAL_EMAIL_SUBJECT:"General Support for FBLA Chapter App",
 
   // Version
   VERSION:"202209.04.22.13",
